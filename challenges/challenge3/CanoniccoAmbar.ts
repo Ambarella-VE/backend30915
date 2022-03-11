55e02fdb7a669e597d6d84e77a6b94723169bf52
Ambar Canonicco <64762162+Ambarella-VE@users.noreply.github.com>
2022-03-10 22:36:19 -0300

    Challenge3

--- challenges/challenge3/CanoniccoAmbar.ts
+++ challenges/challenge3/CanoniccoAmbar.ts
@@ -12,39 +12,11 @@ const listen = clc.blueBright.italic
 /*                   Functions                  */
 /* -------------------------------------------- */
 
-import fs from "fs";
-
-// function getProducts (path: string): Array<Object> {
-//   let productsArray: Array<Object> ;
-//   fs.promises.readFile(path, 'utf-8')
-//   .then( content => productsArray = JSON.parse(content))
-//   .catch( error => {
-//     log(warn('getProducts error'));
-//     throw error
-//   })
-//   return productsArray
-// }
-
 function getRandomInt(min: number, max: number) : number {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-// function getRandomProduct (path: string): Object {
-//   let product: Object;
-//   let products: Array<Object>
-//   let contentLength: number;
-//   fs.promises.readFile (path, 'utf-8')
-//   .then( content => {
-//     products = JSON.parse(content)
-//     contentLength = products.length
-//     product = products[getRandomInt(0,contentLength)];
-//   })
-//   .catch( error => {
-//     log(warn('getRandomProduct error'));
-//     throw error
-//   })
-//   return product
-// }
+
 
 /* -------------------------------------------- */
 /*                 Server Setup                 */
